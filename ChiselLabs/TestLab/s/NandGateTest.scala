a772880d432ef9fa4c976974dee84661ba89021e
SaadAliHafiz <59177839+SaadAliHafiz@users.noreply.github.com>
2021-10-02 12:24:57 -0700

    Add files via upload

---
+++ ChiselLabs/TestLab/s/NandGateTest.scala
@@ -0,0 +1,17 @@
+package example
+
+import org.scalatest._
+import chiseltest._
+import chisel3._
+
+class NandGateTest extends FreeSpec with ChiselScalatestTester {
+    "And Gate Test" in {
+        test(new NandGate()){c =>
+        c.io.a.poke(1.U)
+        c.io.b.poke(0.U)
+        c.clock.step(1)
+        c.io.c.expect(1.U)
+
+        }
+    }
+}
