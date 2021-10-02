ed64a003876c10b6acdeacaa832f73b5e969c762
SaadAliHafiz <59177839+SaadAliHafiz@users.noreply.github.com>
2021-10-02 12:34:28 -0700

    Add files via upload

---
+++ ChiselLabs/TestLab/s/XorGateTest.scala
@@ -0,0 +1,17 @@
+package example
+
+import org.scalatest._
+import chiseltest._
+import chisel3._
+
+class XorGateTest extends FreeSpec with ChiselScalatestTester {
+    "Xor Gate Test" in {
+        test(new XorGate()){c =>
+        c.io.a.poke(0.U)
+        c.io.b.poke(1.U)
+        c.clock.step(1)
+        c.io.c.expect(1.U)
+
+        }
+    }
+}
