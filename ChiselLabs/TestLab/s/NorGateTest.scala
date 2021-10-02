6b132c94808af8f43c17de9a6ccba51565ad8498
SaadAliHafiz <59177839+SaadAliHafiz@users.noreply.github.com>
2021-10-02 12:25:21 -0700

    Add files via upload

---
+++ ChiselLabs/TestLab/s/NorGateTest.scala
@@ -0,0 +1,17 @@
+package example
+
+import org.scalatest._
+import chiseltest._
+import chisel3._
+
+class NorGateTest extends FreeSpec with ChiselScalatestTester {
+    "Nor Gate Test" in {
+        test(new NorGate()){c =>
+        c.io.a.poke(1.U)
+        c.io.b.poke(0.U)
+        c.clock.step(1)
+        c.io.c.expect(0.U)
+
+        }
+    }
+}
