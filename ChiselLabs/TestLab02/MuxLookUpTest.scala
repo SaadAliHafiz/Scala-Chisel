a1cd6660f3d0dc172e347257a308a766afd8e23e
Saad Ali <59177839+SaadAliHafiz@users.noreply.github.com>
2021-10-10 05:45:03 -0700

    Add files via upload

---
+++ ChiselLabs/TestLab02/MuxLookUpTest.scala
@@ -0,0 +1,23 @@
+package example
+
+import org.scalatest._
+import chiseltest._
+import chisel3._
+
+class MuxLookUpTest extends FreeSpec with ChiselScalatestTester {
+    "Mux Look up" in {
+        test(new MuxLookup){c =>
+        c.io.in0.poke(0.B)
+        c.io.in1.poke(1.B)
+        c.io.in2.poke(0.B)
+        c.io.in3.poke(1.B)
+        c.io.in4.poke(0.B)
+        c.io.in5.poke(1.B)
+        c.io.in6.poke(0.B)
+        c.io.in7.poke(1.B)
+        c.io.sel.poke("b101".U)
+        c.clock.step(1)
+        c.io.out.expect(1.B)
+        }
+    }
+}
