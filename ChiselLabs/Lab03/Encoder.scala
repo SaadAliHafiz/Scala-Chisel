ab70954321b3b625f78384cba0e3eb1c4e25e634
Saad Ali <59177839+SaadAliHafiz@users.noreply.github.com>
2021-10-12 08:18:57 -0700

    Add files via upload

---
+++ ChiselLabs/Lab03/Encoder.scala
@@ -0,0 +1,20 @@
+package Lab03
+import chisel3._
+import chisel3.util._
+class EncoderIO extends Bundle {
+val in = Input(UInt(4.W))
+val out = Output(UInt(2.W))
+}
+
+class Encoder extends Module{
+val io = IO ( new EncoderIO)
+when(io.in === "b0001".U){
+io.out := "b00".U
+}.elsewhen(io.in==="b0010".U) {
+io.out :="b01".U
+}.elsewhen(io.in === "b0100".U ) {
+io.out := "b10".U
+}.otherwise {
+io.out := "b11".U
+}
+}
